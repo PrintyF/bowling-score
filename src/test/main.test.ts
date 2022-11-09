5eaea94da90c6f9f15b65077cdc255330f95e28d
Printyf <felix.pinty@gmail.com>
2022-11-09 12:11:23 +0100

    Add test one player one throw two pins

--- src/test/main.test.ts
+++ src/test/main.test.ts
@@ -4,8 +4,12 @@ describe('bowling score', ()=> {
 
     describe('one player', ()=> {
 
-        it('one throw', ()=> {
+        it('one throw one pin', ()=> {
             expect(bowlingScore([1])).toEqual([1]);
+        });
+
+        it('one throw two pins', ()=> {
+            expect(bowlingScore([2])).toEqual([2]);
         });
     });
 });
