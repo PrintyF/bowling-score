3c6da9fd269ee817674ed2fc57be3693c1e900fd
Printyf <felix.pinty@gmail.com>
2022-11-09 17:09:51 +0100

    Add test with two strike

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 export function bowlingScore(scores: Array<Array<number>>): Array<number> {
     let strikeIndex = scores[0].findIndex((el)=> el === 10)
-    if (strikeIndex != -1) {
-        let value = scores[0][strikeIndex];
-        value += scores[0][strikeIndex + 1] * 2;
-        if (scores[0][strikeIndex + 2])
-            value += scores[0][strikeIndex + 2] * 2;
-        return [value];
-    }
-    return  [scores[0].reduce((a, b) => a + b )];
+    let bonus = false;
+    return  [scores[0].reduce((a, b, index) => {
+        if (strikeIndex === index - 1) {
+            strikeIndex = scores[0].slice(strikeIndex + 1).findIndex((el)=> el === 10) + strikeIndex + 1
+            bonus = true;
+            return a + b * 2;
+        }
+       else if (bonus) {
+            bonus = false;
+            return a + b * 2;
+        }
+        return a + b;
+    })];
 }
--- src/test/main.test.ts
+++ src/test/main.test.ts
@@ -35,7 +35,7 @@ describe('bowling score', ()=> {
                 });
 
                 it('one pin then two pins', ()=> {
-                    expect(bowlingScore([[1, 10, 2, 0]])).toEqual([14]);
+                    expect(bowlingScore([[1, 10, 2, 0]])).toEqual([15]);
                 });
 
                 it('three pins on two throws after strike', ()=> {
@@ -44,6 +44,17 @@ describe('bowling score', ()=> {
 
                 it('three pins on one throw but with insufficient throw', ()=> {
                     expect(bowlingScore([[10, 3]])).toEqual([16]);
+                });
+
+                it('just a strike', ()=> {
+                    expect(bowlingScore([[10]])).toEqual([10]);
+                });
+
+            });
+
+            describe('strikes', () => {
+                it('two classic strikes', ()=> {
+                    expect(bowlingScore([[10, 1, 2, 10, 1, 2]])).toEqual([32]);
                 });
 
             });
