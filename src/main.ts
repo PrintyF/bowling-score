5864477126488fe99df118201dcdcb89e59ec9cf
Printyf <felix.pinty@gmail.com>
2022-11-09 14:20:29 +0100

    refacto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 export function bowlingScore(scores: Array<Array<number>>): Array<number> {
-    if (scores[0].find((el)=> el === 10)) {
-        if (scores[0][1] === 2)
-            return [14];
-        return [12];
+    let strike = scores[0].find((el)=> el === 10)
+    if (strike) {
+        return [scores[0][0] + scores[0][1] * 2];
     }
     return  [scores[0].reduce((a, b) => a + b )];
 }
